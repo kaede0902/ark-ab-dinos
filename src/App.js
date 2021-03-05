8c7895ccbe03128390693f9f41743c3f95fe8c1c
kaede0902 <kaede0920js@gmail.com>
2021-03-06 08:09:04 +0900

    add i18n system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import { useTranslation } from 'react-i18next'
+import React, { useState, useEffect } from 'react';
+
+import enJson from './locales/en.json'
+import jaJson from './locales/ja.json'
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: enJson,
+    },
+    ja: {
+      translation: jaJson,
+    },
+  },
+  lng: 'en',
+  fallback: 'en',
+  interpolation: {escapeValue: false},
+});
 
 function App() {
+  const [t, i18n] = useTranslation();
+  const [lang, setLang] = useState('en');
+
+  useEffect( () => {
+    i18n.changeLanguage(lang)
+  } ,[lang, i18n]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{t('welcome')}</p> 
+      <div>
+        <button onClick={
+          () => setLang(lang === 'en' ? 'ja' : 'en')
+        }>
+          {t('changeLang')}
+        </button>
+      </div>
     </div>
   );
 }
